a049f3dfbee5060db8d2176a7272a9fd648fc853
Daniel RB <daniel@danielrb.dev>
2021-01-14 15:40:50 -0500

    remove nose and achieve 100% coverage

--- guamapa/users/test/test_manager.py
+++ guamapa/users/test/test_manager.py
@@ -40,3 +40,6 @@ class UsersManagersTests(TestCase):
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
                 email='super@user.com', password='foo', is_superuser=False)
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='super@user.com', password='foo', is_staff=False)
